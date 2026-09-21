aab711ca9aff1c1efdb9bbc248b77655055d6387
Pedro Luka <pedrolukagt@gmail.com>
2026-09-21 14:15:09 -0300

    first commit

---
+++ src/main/java/antifraud/motorrisco/MotorRiscoApplication.java
@@ -0,0 +1,13 @@
+package antifraud.motorrisco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotorRiscoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MotorRiscoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/antifraud/motorrisco/MotorRiscoApplicationTests.java
@@ -0,0 +1,13 @@
+package antifraud.motorrisco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotorRiscoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
