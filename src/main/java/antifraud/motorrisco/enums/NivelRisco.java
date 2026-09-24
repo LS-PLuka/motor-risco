d81b1d535a339f7eab0b0e50396c91c25e29fcfa
Pedro Luka <pedrolukagt@gmail.com>
2026-09-24 11:57:28 -0300

    feat: retorna regras que foram aplicadas

--- src/main/java/antifraud/motorrisco/enums/NivelRisco.java
+++ src/main/java/antifraud/motorrisco/enums/NivelRisco.java
@@ -3,5 +3,17 @@ package antifraud.motorrisco.enums;
 public enum NivelRisco {
     APROVADA,
     SINALIZADA,
-    BLOQUEADA
+    BLOQUEADA;
+
+    public static NivelRisco classificar(int pontuacao) {
+        if (pontuacao >= 70) {
+            return BLOQUEADA;
+        }
+
+        if (pontuacao >= 40) {
+            return SINALIZADA;
+        }
+
+        return APROVADA;
+    }
 }
